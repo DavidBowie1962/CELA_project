7418c0af7b1ab942d4b37ec1084372eec72ebb78
DavidBowie <72200248+DavidBowie1962@users.noreply.github.com>
2022-06-01 11:40:09 +0300

    Delete creds.py

--- creds.py
+++
@@ -1,8 +0,0 @@
-token = "" # insert your own token
-account_id = '2025863826'
-class_code_bonds = "TQOB"
-class_code_shares = "TQBR"
-class_code_spb = 'SPBXM'
-
-#https://www.moex.com/ru/issue.aspx?board=FQBR&code=TSLA-RM&utm_source=www.moex.com&utm_term=tesla
-#https://spbexchange.ru/ru/listing/securities/list/
